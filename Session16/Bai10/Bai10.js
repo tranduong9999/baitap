5213f71bfdb3968710b77a7847ac906053a8f3fd
tranduong9999 <tranduognhihi@gmail.com>
2025-02-26 11:47:52 +0700

    adc

--- Session16/Bai10/Bai10.js
+++ Session16/Bai10/Bai10.js
@@ -1,4 +1,4 @@
 let num1 = parseFloat(prompt("Nhập số thứ nhất:"));
 let num2 = parseFloat(prompt("Nhập số thứ hai:"));
-let randomNumber = Math.random(num1,num2)
+let randomNumber = Math.random() * (num2 - num1 ) + num1;
 alert(randomNumber);
